77fb822cd03c73bc7dcc8d732dfabeaec7354ce9
Boris Alennikov <AlennikovBoris@gmail.com>
2021-09-19 21:00:10 +0300

    test

---
+++ cmake-build-debug/CMakeFiles/Lab__2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab__2.dir/main.cpp.obj"
+  "Lab__2.exe"
+  "Lab__2.exe.manifest"
+  "Lab__2.lib"
+  "Lab__2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab__2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
